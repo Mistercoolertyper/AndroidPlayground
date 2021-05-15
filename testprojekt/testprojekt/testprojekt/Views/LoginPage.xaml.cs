dc4e690d2612dd9d3b17cc85650e9ebaa812bd6b
BuildTools <unconfigured@null.spigotmc.org>
2021-05-15 18:00:47 +0200

    Login page almost working Note: gotta find out how to make it redirect to other page

--- testprojekt/testprojekt/testprojekt/Views/LoginPage.xaml.cs
+++ testprojekt/testprojekt/testprojekt/Views/LoginPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using testprojekt.Models;
 using testprojekt.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +16,19 @@ namespace testprojekt.Views
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        void SignInPrecedure(object sender, EventArgs e) 
+        {
+            User user = new User(Entry_Username.Text, Entry_Password.Text);
+            if (user.CheckInformation())
+            {
+                DisplayAlert("Login", "Login Success", "OK");
+            }
+            else 
+            {
+                DisplayAlert("Login", "Login Not Correct, empty username or password.", "OK");
+            }
         }
     }
 }
